388d5719fed3f0f1fb605265fe55969ef553435e
Arianna Carotti <arianna.carotti@gmail.com>
2021-04-08 17:42:33 +0200

    provato con funzioni

--- js/script.js
+++ js/script.js
@@ -5,12 +5,59 @@
 var rightArrow = $('.arrow.right');
 console.log(rightArrow);
 
+// al click sulle frecce rendiamo visibile l'immagine successiva 
 rightArrow.click(function () {    
 
     var imgActive = $('.active');
-    console.log(imgActive);
 
-    imgActive.removeClass('active');
-    
+    var nextImg = imgActive.next();
+
+    rightClick(imgActive, nextImg);   
 
 });
+
+var leftArrow = $('.arrow.left');
+
+leftArrow.click(function () {  
+
+    var imgActive = $('.active');
+
+    var previousImg = imgActive.prev();
+    
+    leftClick(imgActive, previousImg); 
+});
+
+
+
+
+
+// FUNZIONI
+//
+//
+function rightClick(img, nextone) {
+    console.log(img);
+
+    img.removeClass('active');     
+    
+    if ( img.hasClass('last')) {
+
+        $('.first').addClass('active')
+    }else {
+        nextone.addClass('active')
+    }
+
+}
+
+function leftClick(img, previous) {
+    console.log(img);
+
+    img.removeClass('active');     
+    
+    if ( img.hasClass('first')) {
+
+        $('.last').addClass('active')
+    }else {
+        previous.addClass('active')
+    }
+
+}
