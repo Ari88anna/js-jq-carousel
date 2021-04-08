1370fde85cae119f090fc2590f0b478e79cc4c10
Arianna Carotti <arianna.carotti@gmail.com>
2021-04-08 16:49:37 +0200

    push per controllo

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@
 // Lo slider non deve mai rompersi, ma se l'immagine visibile è l'ultima e clicco sulla freccia a destra lo slider mostra la prima.
 // Se l'immagine visibile è la prima e clicco sulla freccia a sinistra, lo slider mostra l'ultima.
 
+var rightArrow = $('.arrow.right');
+console.log(rightArrow);
+
+rightArrow.click(function () {    
+
+    var imgActive = $('.active');
+    console.log(imgActive);
+
+    imgActive.removeClass('active');
+    
+
+});
