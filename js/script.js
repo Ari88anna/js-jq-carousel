052b121291ec6690c6eb74d7b062e38122e588d7
Arianna Carotti <arianna.carotti@gmail.com>
2021-04-08 15:48:16 +0200

    creato html e css

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Slider. Al click sulla sulle frecce rendiamo visibile l'immagine successiva o precedente (in base alla freccia sulla quale ho cliccato).
+// Lo slider non deve mai rompersi, ma se l'immagine visibile è l'ultima e clicco sulla freccia a destra lo slider mostra la prima.
+// Se l'immagine visibile è la prima e clicco sulla freccia a sinistra, lo slider mostra l'ultima.
+
